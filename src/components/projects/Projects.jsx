e9657482c78dd39f4f4d7b489ceb077f1f4f18c3
Rozimurodov <egouzbekistan@Rozimurodov.local>
2024-08-02 19:58:22 +0500

    change

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,43 +13,39 @@ function Projects() {
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/castpress.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">castpress</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://castpress.netlify.app">castpress</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/felix.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">felix</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://marketcars.netlify.app">felix</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/gradient.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">gradient color</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://randomgradientcolor.netlify.app">gradient color</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/lnkhouse.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/"> lnkhouse</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://lnkhouse.netlify.app"> lnkhouse</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/scroll.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">Scroll image</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://imagescrolll.netlify.app">Scroll image</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/search.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">search panel</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://searchpaneel.netlify.app">search panel</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/speed.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">need for speed</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://forspeed.netlify.app">need for speed</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/trafalgar.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">trafalgar</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://trafallgarr.netlify.app">trafalgar</a>
           </div>
           <div className="text-center">
             <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/user.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">add user</a>
-          </div>
-          <div className="text-center">
-            <img className="w-80 h-auto rounded-xl cursor-pointer mb-3 border-2" src="./public/portfolio.png" alt=""/>
-            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://artell.netlify.app/">portfolio</a>
+            <a target="blank"className="text-xl font-bold text-2xl capitalize" href="https://userstodolists.netlify.app">add user</a>
           </div>
         </div>
       </div>
